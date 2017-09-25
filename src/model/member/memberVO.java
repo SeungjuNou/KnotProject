7c01ad4434cc0db97e4d04527179a2516941866d
JAYS\Jay <jwelisabeth@gmail.com>
2017-09-25 19:19:11 +0900

    memberVO, qnaVo 작성

--- src/model/member/memberVO.java
+++ src/model/member/memberVO.java
@@ -1,5 +1,91 @@
 package model.member;
 
+import java.util.Date;
+
 public class memberVO {
 
+	private int mem_no;
+	private String mem_id;
+	private String mem_pwd;
+	private String mem_name;
+	private Date mem_birth;
+	private String mem_phone;
+	private String mem_img;
+	private String memo;
+	private String mem_area_code;
+	private int mem_lev;
+	private Date mem_date;
+	
+	
+	public int getMem_no() {
+		return mem_no;
+	}
+	public void setMem_no(int mem_no) {
+		this.mem_no = mem_no;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getMem_pwd() {
+		return mem_pwd;
+	}
+	public void setMem_pwd(String mem_pwd) {
+		this.mem_pwd = mem_pwd;
+	}
+	public String getMem_name() {
+		return mem_name;
+	}
+	public void setMem_name(String mem_name) {
+		this.mem_name = mem_name;
+	}
+	public Date getMem_birth() {
+		return mem_birth;
+	}
+	public void setMem_birth(Date mem_birth) {
+		this.mem_birth = mem_birth;
+	}
+	public String getMem_phone() {
+		return mem_phone;
+	}
+	public void setMem_phone(String mem_phone) {
+		this.mem_phone = mem_phone;
+	}
+	public String getMem_img() {
+		return mem_img;
+	}
+	public void setMem_img(String mem_img) {
+		this.mem_img = mem_img;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	public String getMem_area_code() {
+		return mem_area_code;
+	}
+	public void setMem_area_code(String mem_area_code) {
+		this.mem_area_code = mem_area_code;
+	}
+	public int getMem_lev() {
+		return mem_lev;
+	}
+	public void setMem_lev(int mem_lev) {
+		this.mem_lev = mem_lev;
+	}
+	public Date getMem_date() {
+		return mem_date;
+	}
+	public void setMem_date(Date mem_date) {
+		this.mem_date = mem_date;
+	}
+	
+	
+	
+	
+	
 }
