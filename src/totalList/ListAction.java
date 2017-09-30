4aac76396578dc060c18d3a1bb8a67c3a50bd615
DESKTOP-9N7AFIV\박미영 <604611m@gmail.com>
2017-09-30 15:40:48 +0900

    qna list, write, view

--- src/totalList/ListAction.java
+++ src/totalList/ListAction.java
@@ -71,9 +71,9 @@ public class ListAction extends ActionSupport {
 	
 //Q&A 게시글을 불러오는 메서드
 	public List qnaList() throws Exception { 
-		/*blockCount = 10;
+		blockCount = 10;
 		list = new ArrayList<QnaVO>();
-		list = sqlMapper.queryForList("selectQnaAll");*/
+		list = sqlMapper.queryForList("selectQnaAll");
 		return list;
 	}
 	
